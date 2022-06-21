3647acc1d43a8e8e2cfdbcfc29685e3b6123c2df
Ksenia <52448563+kseniarr@users.noreply.github.com>
2022-06-21 16:25:42 +0300

    Update consts.js

--- app/config/consts.js
+++ app/config/consts.js
@@ -2,8 +2,8 @@ export default {
     borderRadius: 5,
     bottomPadding: 40,
     baseUrl: "https://www.googleapis.com/",
-    nytApiKey: "FFfJWvHZLXNGlVX8SRPICahCCtedaJT2",
-    googleBooksApiKey: "AIzaSyCQgzhEKHD9q3-UREQGAg3W77KUf898Nuw",
+    nytApiKey: "",
+    googleBooksApiKey: "",
     loadingStates: {
         "INITIAL": "INITIAL",
         "SUCCESS": "SUCCESS",
